bcc1b6061c1e9305e6b995eb7ca0c8df17bd20d1
paul rimiru <paulrimiru@gmail.com>
2017-10-08 18:35:00 +0300

    [FIX] Fix database paths

--- instance/config.py
+++ instance/config.py
@@ -7,23 +7,20 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = 'secret'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:10131994@localhost/virs_db'
-    DATABASE_URL = 'postgres://agjlhnyhexnjjp:71aaff89c40cf61f2fd6f2773a0235b296cf1dd862a5a8abcbe7cfb1a00195c6@ec2-50-17-217-166.compute-1.amazonaws.com:5432/daktgc3ebm2uj2'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://mike:10131994@localhost/virs_db')
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
     CSRF_ENABLED = True
     SECRET = 'secret'
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:10131994@localhost/virs_db'
-    DATABASE_URL = 'postgres://agjlhnyhexnjjp:71aaff89c40cf61f2fd6f2773a0235b296cf1dd862a5a8abcbe7cfb1a00195c6@ec2-50-17-217-166.compute-1.amazonaws.com:5432/daktgc3ebm2uj2'
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://mike:10131994@localhost/virs_db')
 class TestingConfig(Config):
     """Configurations for Testing, with a separate test database."""
     CSRF_ENABLED = True
     SECRET = 'secret'
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mike:10131994@localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql+psycopg2://mike:10131994@localhost/test_db')
     DEBUG = True
 
 class StagingConfig(Config):
