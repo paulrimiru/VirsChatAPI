d4dcb9a2fc318d7644941d9f3030ae06b19f9ba6
paul rimiru <paulrimiru@gmail.com>
2017-10-08 11:53:34 +0300

    [FIX] fixing app to run on heroku

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 from flask_failsafe import failsafe
 
-
-@failsafe
-def create_app():
-    from app import app
-    return app
+from app import app
 
 if __name__ == '__main__':
-    create_app().run()
+    app.run(host='0.0.0.0')
